270c14074041aea7e27adf22464a7eea3e7f5afe
YourGitHubUsername <YourGitHubEmail>
2026-07-13 21:56:26 +0200

    wabakadabra

---
+++ Python/ToDo/View.py
@@ -0,0 +1,27 @@
+﻿import tkinter as tk
+
+class ToDoView:
+    def __init__(self):
+        self.okno = tk.Tk()
+        self.okno.title("ToDo App")
+        self.okno.geometry("400x500")
+
+
+
+        self.okvir_gumbov = tk.Frame(self.okno)
+        self.okvir_gumbov.pack(side = tk.TOP, pady=10)
+        self.button1 = tk.Button(self.okvir_gumbov, text="Dodaj", width=10)
+        self.button1.pack(side=tk.LEFT, padx=5)
+        self.button2 = tk.Button(self.okvir_gumbov, text="Delete",  width=10)
+        self.button2.pack(side=tk.LEFT, padx=5)
+        self.button3 = tk.Button(self.okvir_gumbov, text="Done",  width=10)
+        self.button3.pack(side=tk.LEFT, padx=5)
+
+        self.vnos = tk.Entry(self.okno, width=30)
+        self.vnos.pack(pady=30)
+
+        self.ListBox = tk.Listbox(self.okno, width=300, height=250)
+        self.ListBox.pack(side=tk.BOTTOM, pady=30, padx = 20)
+
+    def zazeni(self):
+        self.okno.mainloop()
---
+++ Python/ToDo/main.py
@@ -0,0 +1,27 @@
+﻿from View import ToDoView
+
+class ToDoLogika:
+    def __init__(self):
+        # Tukaj bomo imeli seznam opravil v spominu
+        self.opravila = []
+
+        # Ustvarimo vmesnik in mu podamo SEBE (self), torej celoten razred z logiko
+        self.vmesnik = ToDoView(self)
+
+    def dodaj_opravilo(self):
+        print("Logika: Dodajam opravilo...")
+
+    def brisi_opravilo(self):
+        print("Logika: Brišem opravilo...")
+
+    def opravljeno_opravilo(self):
+        print("Logika: Označujem kot opravljeno...")
+
+    def zazeni(self):
+        self.vmesnik.zazeni()
+
+
+# Zagon aplikacije
+if __name__ == "__main__":
+    aplikacija = ToDoLogika()
+    aplikacija.zazeni()
