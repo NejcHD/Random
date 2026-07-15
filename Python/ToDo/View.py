b6020dd2cf772bb7f672f0d38c2c3d7f1bf04906
skrttt <nejc.prezelj.ng@gmail.com>
2026-07-15 16:43:48 +0200

    dodano urejanje

--- Python/ToDo/View.py
+++ Python/ToDo/View.py
@@ -13,19 +13,32 @@ class ToDoView:
 
         self.okvir_gumbov = tk.Frame(self.okno)
         self.okvir_gumbov.pack(side = tk.TOP, pady=10)
-        self.button1 = tk.Button(self.okvir_gumbov, text="Dodaj", width=10, command=self.logika.dodaj_opravilo)
+        self.button1 = tk.Button(self.okvir_gumbov, text="Dodaj", width=8, command=self.logika.dodaj_opravilo)
         self.button1.pack(side=tk.LEFT, padx=5)
-        self.button2 = tk.Button(self.okvir_gumbov, text="Delete",  width=10, command=self.logika.brisi_opravilo)
+        self.button2 = tk.Button(self.okvir_gumbov, text="Delete",  width=8, command=self.logika.brisi_opravilo)
         self.button2.pack(side=tk.LEFT, padx=5)
-        self.button3 = tk.Button(self.okvir_gumbov, text="Done",  width=10, command=self.logika.opravljeno_opravilo)
+        self.button3 = tk.Button(self.okvir_gumbov, text="Done",  width=8, command=self.logika.opravljeno_opravilo)
         self.button3.pack(side=tk.LEFT, padx=5)
+        self.button_uredi = tk.Button(self.okvir_gumbov, text="Uredi", width=8, command=self.logika.uredi_opravilo)
+        self.button_uredi.pack(side=tk.LEFT, padx=3)
 
         self.okvir_vnosa = tk.Frame(self.okno)
         self.okvir_vnosa.pack(pady = 20)
-        self.vnos = tk.Entry(self.okvir_vnosa, width=30)
+        self.vnos = tk.Entry(self.okvir_vnosa, width=18)
         self.vnos.pack(pady=30, side=tk.LEFT, padx=10)
-        self.vnosDatum = DateEntry(self.okvir_vnosa, width=15, date_pattern='dd.mm.yyyy')
-        self.vnosDatum.pack(pady=10, padx=10, side=tk.RIGHT)
+        self.vnosDatum = DateEntry(self.okvir_vnosa, width=10, date_pattern='dd.mm.yyyy')
+        self.vnosDatum.pack(pady=10, padx=10, side=tk.LEFT)
+
+
+        self.prioriteta = tk.StringVar(value="Srednja")
+        self.izbira_prioritete = tk.OptionMenu(
+            self.okvir_vnosa,
+            self.prioriteta,
+            "Nizka",
+            "Srednja",
+            "Visoka"
+        )
+        self.izbira_prioritete.pack(side=tk.LEFT, padx=10)
 
         self.ListBox = tk.Listbox(self.okno, width=300, height=250)
         self.ListBox.pack(side=tk.BOTTOM, pady=30, padx = 20)
--- Python/ToDo/main.py
+++ Python/ToDo/main.py
@@ -1,40 +1,91 @@
 ﻿from View import ToDoView
 
+
 class ToDoLogika:
     def __init__(self):
-
         # Tukaj bomo imeli seznam opravil v spominu
         self.opravila = []
+
+        self.indeks_za_urejanje = None
 
         # v vmesnik shranimo celo logiko
         self.vmesnik = ToDoView(self)
 
     def dodaj_opravilo(self):
         print("Logika: Dodajam opravilo...")
-        novo_opravilo = self.vmesnik.vnos.get()
+        novo_opravilo = self.vmesnik.vnos.get().strip()
         datum = self.vmesnik.vnosDatum.get()
-        opraviloDatum = f"{novo_opravilo} - {datum}"
+        izbrana_prioriteta = self.vmesnik.prioriteta.get()
+
+        opravilo_datum = f"{izbrana_prioriteta} -- {novo_opravilo} - {datum}"
+
         if novo_opravilo != "":
-            self.vmesnik.ListBox.insert("end", opraviloDatum )
+            if self.indeks_za_urejanje is None:
+                self.vmesnik.ListBox.insert("end", opravilo_datum)
+            else:
+                self.vmesnik.ListBox.delete(self.indeks_za_urejanje)
+                self.vmesnik.ListBox.insert(self.indeks_za_urejanje, opravilo_datum)
+                self.indeks_za_urejanje = None
+                self.vmesnik.button1.config(text="Dodaj")
+
+            self.vmesnik.vnos.delete(0, "end")
         else:
             print("Vnos je prazen")
 
     def brisi_opravilo(self):
         print("Logika: Brišem opravilo...")
-        izbrano = self.vmesnik.ListBox.curselection()   #vrne izbran idex
+        izbrano = self.vmesnik.ListBox.curselection()
 
         if izbrano:
             indeks = izbrano[0]
             text = self.vmesnik.ListBox.get(indeks)
             print(f"Izbrisano {text}")
             self.vmesnik.ListBox.delete(indeks)
+
+            if self.indeks_za_urejanje == indeks:
+                self.indeks_za_urejanje = None
+                self.vmesnik.button1.config(text="Dodaj")
         else:
             print("Ni izbranega opravila")
 
     def opravljeno_opravilo(self):
         print("Logika: Označujem kot opravljeno...")
         izbrano = self.vmesnik.ListBox.curselection()
-        self.vmesnik.ListBox.itemconfig(izbrano, bg="lightgreen")
+
+        if izbrano:
+            indeks = izbrano[0]
+            self.vmesnik.ListBox.itemconfig(indeks, bg="lightgreen")
+        else:
+            print("Ni izbranega opravila")
+
+    def uredi_opravilo(self):
+        print("Logika: Urejanje opravila...")
+        izbrano = self.vmesnik.ListBox.curselection()
+
+        if not izbrano:
+            print("Ni izbranega opravila za urejanje")
+            return
+
+        indeks = izbrano[0]
+        opravilo = self.vmesnik.ListBox.get(indeks)
+
+        try:
+            prioriteta, ostalo = opravilo.split(" -- ", 1)
+            naziv, datum = ostalo.rsplit(" - ", 1)
+        except ValueError:
+            print("Opravilo ni v pričakovani obliki")
+            return
+
+        self.indeks_za_urejanje = indeks
+
+        self.vmesnik.vnos.delete(0, "end")
+        self.vmesnik.vnos.insert(0, naziv)
+
+        self.vmesnik.vnosDatum.delete(0, "end")
+        self.vmesnik.vnosDatum.insert(0, datum)
+
+        self.vmesnik.prioriteta.set(prioriteta)
+        self.vmesnik.button1.config(text="Shrani")
 
     def zazeni(self):
         self.vmesnik.zazeni()
